d2bef6dcc2fa14dc4029aa2598df9d64db305159
letangileri <let.angileri@gmail.com>
2025-08-04 19:23:51 +0200

    update

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 const urlApi = 'https://flynn.boolean.careers/exercises/api/random/mail';
 
-axios.get(urlApi)
-    .then(response => {
-        console.log(response);
-        
-        const result = response.data.response;
+const emailList = [];
+for (let i = 0; i < 10; i++){
+    axios.get(urlApi)
+        .then(response => {
+            // console.log(response);
+            
+            const result = response.data.response;
 
-        console.log(response.data.response);
-        
-        console.log(result);
-        
-    })
+            // console.log(response.data.response);
+            
+            console.log(result);
+            emailList.push(result)
+            
+        })
+
+}
+console.log(emailList);
