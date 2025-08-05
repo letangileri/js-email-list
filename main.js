6ed8852266b7faf8736b5a7c0d387e78e71975e8
letangileri <let.angileri@gmail.com>
2025-08-05 10:30:18 +0200

    update

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const urlApi = 'https://flynn.boolean.careers/exercises/api/random/mail';
 
-const emailList = [];
+let emailList = [];
 for (let i = 0; i < 10; i++){
     axios.get(urlApi)
         .then(response => {
@@ -12,11 +12,40 @@ for (let i = 0; i < 10; i++){
             
             console.log(result);
             emailList.push(result)
-            console.log(emailList);
+            // document.writeln(emailList);
+            
             
             if (emailList.length == 10){
-                console.log(emailList);
+                // document.writeln(emailList);
             }
         })
 }
 console.log(emailList);
+
+// Inserire un bottone che al click faccia il fetch altre 10 mail (sostituendo le altre)
+let emailList2 = [];
+
+const elemButton = document.getElementById("getEmail")
+const ulElem = document.getElementById('email-list')
+elemButton.addEventListener('click',
+    function (){
+        console.log("ciao");
+        for (let i = 0; i < 10; i++){
+            axios.get(urlApi)
+            .then(response => {
+                const result = response.data.response;
+                emailList2.push(result)
+                console.log(emailList2);
+                ulElem.innerHTML = `<li> ${result} </li>`
+
+                // if (emailList2.length == 10){
+                //     console.log("ciao");
+                    
+                //     ulElem.innerHTML = `<li> ${result} </li>`
+                // }
+                // document.writeln(result);
+                
+            })
+        }
+    }
+)
